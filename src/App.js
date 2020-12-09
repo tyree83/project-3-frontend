e2bb9d9913b9e8e741e719e66612cee7dc8f27af
Tyree Christensen <tyree83@gmail.com>
2020-12-09 12:36:36 -0500

    finishes redirect route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import DashboardPage from './pages/DashboardPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
-import { Switch, Route, withRouter } from 'react-router-dom';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 
 import { getUser, logout } from './services/userService';
 
@@ -41,8 +41,11 @@ function App(props) {
           <Route exact path="/" render={ props => 
             <HomePage />
           } />
-          <Route exact path="/dashboard" render={ props => 
+          <Route exact path="/dashboard" render={ props =>
+            getUser() ? 
             <DashboardPage />
+            :
+            <Redirect to='/login' />
           } />
           <Route exact path="/signup" render={ props => 
             <SignupPage handleSignupOrLogin={handleSignupOrLogin} />
