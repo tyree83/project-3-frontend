e5b1fbc65eaa5488b3451ef74f55fe114894bcac
Tyree Christensen <tyree83@gmail.com>
2020-12-14 19:56:35 -0500

    updates backend url

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import { setToken, getUserFromToken, removeToken } from './tokenService';
 
-const BASE_URL = 'http://localhost:3001/api/users';
+const BASE_URL = 'https://git.heroku.com/p3-backend.git/api/users';
 
 function signup(user) {
     return fetch(BASE_URL + '/signup', {
