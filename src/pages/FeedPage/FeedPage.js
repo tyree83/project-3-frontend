0f45a1c7525f8492de52f875ac0c7554eaf61bfa
Tyree Christensen <tyree83@gmail.com>
2020-12-15 09:36:21 -0500

    styling feed page

--- src/pages/FeedPage/FeedPage.js
+++ src/pages/FeedPage/FeedPage.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import styles from './FeedPage.module.css';
 
 
@@ -6,20 +5,20 @@ import styles from './FeedPage.module.css';
 export default function FeedPage (props) {
   
     const feed = props.posts.map((post, idx) => (
-      <div key={idx}>
-      <p>{post.whoPosted}&nbsp;<em>says:</em></p>
-      <p>{post.imgURL}</p>
-      <h6>{post.textPost}</h6>
-      <p>Date:{post.datePosted}&nbsp;{post.location}</p>
+      <div className={`${styles.cardIndex}`} key={idx}>
+      <p>&nbsp;{post.whoPosted}&nbsp;<em>says:</em>&nbsp;</p>
+      {/* <p>&nbsp;{post.imgURL}</p> */}
+      <h6>&nbsp;{post.textPost}&nbsp;</h6>
+      <p>&nbsp;Date:{post.datePosted}<br/>{post.location}&nbsp;</p>
     </div>
     ));
     return (
       <div className={styles.Feed}>
         <h1 className='header-footer'>Feed</h1>
         {props.posts.length ? 
-          <div className={`${styles.table} table text-info`}>
+          <div className={`${styles.table}`}>
            
-            <div className='card'>
+            <div className={`${styles.card}`}>
               {feed}
             </div>
           </div>
